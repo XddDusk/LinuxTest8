86ee67027cea60b8d29b8b3b329f6f6ee472a4ce
qpr <qpr1140043674@163.com>
2021-11-02 21:36:24 +0800

    my first commit

---
+++ test2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "my hone dir is $HOME"
+echo "my home dir is `pwd`"
---
+++ test4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read name animal1 animal2 animmal3 
+echo "welcome to our ${name}_zoo"
+echo "we have $animal1,$animal2,$animal3"
